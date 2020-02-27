8dc32eca8b3369dcad0f6c49ac446351ffb2f422
CN18 <codenation@CN18s-MacBook-Pro.local>
2020-02-27 14:26:06 +0000

    Basic about me up and running

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
-import React from "react"
-import "../style/About.css"
+import React from "react";
+import "../style/About.css";
 
 const About = () => {
-    return (
-        <div className = "about">
-            <h1>About</h1>
-        </div>
-    )
-}
+  return (
+    <div className="about">
+      <div className="left">
+        <h1>My Journey into coding...</h1>
+      </div>
+      <div className="right">
+        <p>I always liked the idea of working with commputers and coding. Something about the pure logic of the process appealed to me.</p>
+        <p>In December 2019 I had the opportunity to enroll on one of Code Nation's Develop courses and I was right. I loved it!</p>
+        <p>Fast forward a month to January 2020 and I decided to follow up the Develop course with a Master couse. A full immersive twelve week bootcamp that trains people from novice to junior technical profession.</p>
+        <p>The course is not over yet, but I haven't looked back. I am incredibly passionate about learning new techniques and approaches to problems and find myself building small phone web apps to entertain my step-daughter, or sometimes just to see if I can.</p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
