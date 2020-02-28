91ad6750ebdfa10f5497c4ee463e0291046e027c
CN18 <codenation@CN18s-MacBook-Pro.local>
2020-02-28 10:41:14 +0000

    Updates

---
+++ precache-manifest.2d59d063015b20de701243d63119d362.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61f5928947445b7ca8f2601f39229fc",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "5377e96d9106eaaa0ea6",
+    "url": "/portfolio/static/css/main.a2805011.chunk.css"
+  },
+  {
+    "revision": "c34ab3bf5d6bda26a9fd",
+    "url": "/portfolio/static/js/2.7d2e019a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.7d2e019a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5377e96d9106eaaa0ea6",
+    "url": "/portfolio/static/js/main.66789cc2.chunk.js"
+  },
+  {
+    "revision": "1a95985286676740b556",
+    "url": "/portfolio/static/js/runtime-main.f6eb50f0.js"
+  },
+  {
+    "revision": "2878a62c858eaf4bb0e2b1d16ad2eeaa",
+    "url": "/portfolio/static/media/DiceGame.2878a62c.png"
+  },
+  {
+    "revision": "24fa502699e2579a14a25e26602916c4",
+    "url": "/portfolio/static/media/HangmanP.24fa5026.png"
+  },
+  {
+    "revision": "3370eb25bc6e2a4ca85e503a112eb130",
+    "url": "/portfolio/static/media/Pokemon.3370eb25.png"
+  },
+  {
+    "revision": "902ab7375d64dd4d331c411d50734003",
+    "url": "/portfolio/static/media/Webclone.902ab737.png"
+  }
+]);
