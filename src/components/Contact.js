5e1e7f3cf3b2424504e73932f7e42632a282962c
CN18 <codenation@CN18s-MacBook-Pro.local>
2020-02-28 15:15:27 +0000

    base styling finished

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,14 +5,14 @@ const Contact = () => {
   return (
     <div className="contact">
       <h5 className = "textContact">As a relative newcomer to the tech industry I would love to hear from other people and get their thoughts and feedback. If you want to get in touch or give any feedback then feel free to contact me directly or on LinkedIn.</h5>
-      <h5 className = "textContact">Contact:-</h5>
+      <h5 className = "textContactHead">Contact:-</h5>
       <ul className = "list">
         <li className = "listItem">email address: michael__clare@hotmail.co.uk</li>
-        <li className = "listItem"><a href="https://www.linkedin.com/in/mike-clare/">LinkedIn</a></li>
+        <li className = "listItem"><a href="https://www.linkedin.com/in/mike-clare/">LinkedIn Link</a></li>
       </ul>
       <h4 className = "textContact">Or, just have a look at my github repository and leave any comments. All feedback is most welcome.</h4>
       <ul className = "list">
-        <li className = "listItem"><a href="github.come/mclarework">My github</a></li>
+        <li className = "listItem"><a href="https://github.com/mclarework?tab=repositories">My github repositories</a></li>
       </ul>
     </div>
   );
