7fe3878230f686f05d87a29ed71a1c555cbb2cdc
CN18 <codenation@CN18s-MacBook-Pro.local>
2020-02-28 16:53:42 +0000

    More cube work

--- src/components/Cube.js
+++ src/components/Cube.js
@@ -8,7 +8,7 @@ class Cube extends Component {
         <div class = "main">
         <div class = "cube">
             <div class = "face one">
-                <h1>One</h1>
+                <h1 id = "git_face">git &amp; gihub</h1>
             </div>
             <div class = "face two">
                 <h1 id = "html_face">html</h1>
@@ -17,13 +17,13 @@ class Cube extends Component {
                 <h1 id = "css_face">CSS</h1>
             </div>
             <div class = "face four">
-                <h1>Four</h1>
+                <h1 id = "React_face">React.js</h1>
             </div>
             <div class = "face five">
                 <h1 id = "js_face">Javascript</h1>
             </div>
             <div class = "face six">
-                <h1>Six</h1>
+                <h1 id = "Node_face">Node.js</h1>
             </div>
         </div>
     </div>
